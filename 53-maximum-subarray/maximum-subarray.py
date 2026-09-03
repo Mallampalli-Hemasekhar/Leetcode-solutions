cb45646d7b1d1532eea56dc53e5a9d23449ccda6
Mallampalli-Hemasekhar <hemasekhar522@gmail.com>
2026-09-03 23:12:42 +0530

    Time: 61 ms (74.83%) | Memory: 22 MB (10.70%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        run_sum,max_sum=0,min(nums)
+        for i in nums:
+            run_sum+=i
+            max_sum=max(max_sum,run_sum)
+            if run_sum<0:
+                run_sum=0
+        return max_sum
