28ba3a269707ba1beecaaa25f3fdd4d6083da690
Mallampalli-Hemasekhar <hemasekhar522@gmail.com>
2026-09-16 23:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (21.21%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -4,8 +4,4 @@ class Solution(object):
         :type piles: List[int]
         :rtype: bool
         """
-        m=max(piles)
-        for i in piles:
-            if i%m==0:
-                return True
-        return False
+        return True
